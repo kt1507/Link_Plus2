5b808c14f326f7c9c11d1d0d36316d3c42a81021
JONGTAELEE <66667864+JONGTAELEE@users.noreply.github.com>
2020-08-06 16:17:20 +0900

    로그인 (회원가입) pwd inputtype + checksum 추가 <로그인 연동 확인해야함>

--- app/src/main/java/com/example/linkmain/LoginActivity.java
+++ app/src/main/java/com/example/linkmain/LoginActivity.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,18 +22,19 @@ public class LoginActivity extends AppCompatActivity {
     private EditText email_login;
     private EditText pwd_login;
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // getSupportActionBar().hide();
+        getSupportActionBar().hide();
 
         join = (Button) findViewById(R.id.main_join_btn);
         login = (Button) findViewById(R.id.main_login_btn);
         email_login = (EditText) findViewById(R.id.main_email);
         pwd_login = (EditText) findViewById(R.id.main_pwd);
 
-/*  firebase database 사용시
         firebaseAuth = firebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
@@ -36,23 +44,22 @@ public class LoginActivity extends AppCompatActivity {
                 String pwd = pwd_login.getText().toString().trim();
 
                 firebaseAuth.signInWithEmailAndPassword(email, pwd)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                     startActivity(intent);
-                                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out); // 슬라이딩 애니메이션 화면전환 삽입
+                                    // overridePendingTransition(FADE_IN_ANIMATION, FADE_OUT_ANIMATION); // 슬라이딩 애니메이션 화면전환 사용시 사용
                                 } else {
-                                    Toast.makeText(MainActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "로그인 오류", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
-
             }
         });
-*/
+
         join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/linkmain/SignUpActivity.java
+++ app/src/main/java/com/example/linkmain/SignUpActivity.java
@@ -1,17 +1,28 @@
 package com.example.linkmain;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText email_join;
     private EditText pwd_join;
+    private EditText pwd_check_join;
     private Button btn;
-//    FirebaseAuth firebaseAuth;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,33 +32,42 @@ public class SignUpActivity extends AppCompatActivity {
 
         email_join = (EditText) findViewById(R.id.sign_up_email);
         pwd_join = (EditText) findViewById(R.id.sign_up_pwd);
+        pwd_check_join = (EditText) findViewById(R.id.sign_up_pwd_check);
         btn = (Button) findViewById(R.id.sign_up_btn);
 
-/*        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance(); //firebase 연동
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //입력 정보 정보 가져옴
                 String email = email_join.getText().toString().trim();
                 String pwd = pwd_join.getText().toString().trim();
+                String pwd_check = pwd_check_join.getText().toString().trim();
 
-                firebaseAuth.createUserWithEmailAndPassword(email, pwd)
-                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                                if (task.isSuccessful()) {
-                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    Toast.makeText(SignUpActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
-                                    return;
-                                }
+                if(pwd.equals(pwd_check)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                                // overridePendingTransition(FADE_IN_ANIMATION, FADE_OUT_ANIMATION); // 슬라이딩 애니메이션 화면전환 사용시 사용
+                                finish();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "등록 에러", Toast.LENGTH_SHORT).show();
+                                return;
                             }
-                        });
-            }
-        });
-*/
-    }
-}
+                        }
+                    });
+
+                } // if - pwd equals
+                else{
+                    Toast.makeText(getApplicationContext(), "입력하신 비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG);
+                    return;
+                } // else - pwd equals
+            } // onClick END
+        }); // 버튼 onClickListener END
+    } // onCreate END
+} // public class END
