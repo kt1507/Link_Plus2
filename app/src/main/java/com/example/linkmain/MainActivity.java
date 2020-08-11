6d078e82e6d36ae77d9867e602d267d063841f67
kt1507 <66198199+kt1507@users.noreply.github.com>
2020-08-12 04:40:32 +0900

    슬라이딩 레이아웃 구현
    지금은 listview 안보임
    fragment listview으로 수정해야함

--- app/src/main/java/com/example/linkmain/MainActivity.java
+++ app/src/main/java/com/example/linkmain/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -18,6 +23,15 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    //숨겨진 페이지가 열렸는지 체크
+    boolean isPageOpen = false;
+
+    Animation translateTopAnim;
+    Animation translateBottomAnim;
+
+    ListView page;
+    Button Animbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +58,56 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        page = findViewById(R.id.listview);
+
+        //anim 폴더의 애니메이션 가져와서 준비
+        translateTopAnim = AnimationUtils.loadAnimation(this,R.anim.translate_top);
+        translateBottomAnim = AnimationUtils.loadAnimation(this,R.anim.translate_bottom);
+
+        //페이지 슬라이딩 이벤트가 발생했을 때 애니메이션이 시작 됐는지 종료 됐는지 감지
+        SlidingPageAnimationListener animListener = new SlidingPageAnimationListener();
+
+        translateTopAnim.setAnimationListener(animListener);
+        translateBottomAnim.setAnimationListener(animListener);
+
+        //버튼을 눌렀을 때
+        Animbutton = findViewById(R.id.button);
+        Animbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(isPageOpen){
+                    page.startAnimation(translateTopAnim);
+                }else{
+                    page.setVisibility(View.VISIBLE);
+                    page.startAnimation(translateBottomAnim);
+                }
+            }
+        });
+    }
+
+    private class SlidingPageAnimationListener implements Animation.AnimationListener{
+        @Override
+        public void onAnimationStart(Animation animation){
+
+        }
+
+        public void onAnimationEnd(Animation animation){
+            if(isPageOpen){
+                page.setVisibility(View.INVISIBLE);
+
+                Animbutton.setText("열기");
+                isPageOpen = false;
+            }else{
+                Animbutton.setText("닫기");
+                isPageOpen = true;
+            }
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation){
+
+        }
     }
 
     @Override
